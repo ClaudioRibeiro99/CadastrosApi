4f00397e0c0936957b9bebfb0c701400acd5b64f
Cláudio Marcelo Ribeiro <claudio.ribeiro@lucree.com.br>
2022-05-13 18:48:24 -0300

    Melhoria na propriedade CadastrarUsuario (DataAnnotations)

--- CadastrosApi/Models/CadastrarUsuario.cs
+++ CadastrosApi/Models/CadastrarUsuario.cs
@@ -8,14 +8,21 @@ namespace CadastrosApi.Models
     public class CadastrarUsuario
     {
         public int Id { get; set; }
-
-        [EmailAddress(ErrorMessage = "Email invalido, verifique o email e tente nvoamente")]
-        [Display(Description = "Digite um email")]
+        [Required(ErrorMessage = "Email é obrigatório")]
+        [RegularExpression("^[a-z0-9_\\+-]+(\\.[a-z0-9_\\+-]+)*@[a-z0-9-]+(\\.[ a-z0-9]+)*\\.([az]{2,4})$", ErrorMessage = "Formato de e-mail inválido.")]
         [StringLength(maximumLength:80, MinimumLength = 5)]
         public string Email { get; set; }
+        [Required(ErrorMessage = "Nome é obrigatório")]
+        [StringLength(maximumLength: 15, MinimumLength = 2)]
         public string Nome { get; set; }
+        [Required(ErrorMessage = "Último nome é obrigatório")]
+        [StringLength(maximumLength: 15, MinimumLength = 2)]
         public string UtimoNome { get; set; }
+        [Required(ErrorMessage = "Senha é obrigatório")]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
+        [Required(ErrorMessage = "A confirmação de é obrigatório")]
+        [DataType(DataType.Password)]
         public string ConfirmarPassword { get; set; }
     }
 }
